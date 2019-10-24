7892ffea414f997ae3306bfd7d350cc550d37bd8
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-10-24 18:57:28 +0300

    1 dalis

---
+++ app/Outfit.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Outfit extends Model
+{
+    public function outfitMaster()
+    {
+        return $this->belongsTo('App\Master', 'master_id', 'id');
+    }
+ 
+}
---
+++ resources/views/master/index.blade.php
@@ -0,0 +1,9 @@
+@foreach ($masters as $master)
+<a href="{{route('master.edit',[$master])}}">{{$master->name}} {{$master->surname}}</a><br>
+<form method="POST" action="{{route('master.destroy', [$master])}}">
+    @csrf
+    <button type="submit">DELETE</button>
+   </form>
+   <br>
+ 
+@endforeach
